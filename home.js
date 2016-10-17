0394b171ecc78a8efbf3427a29f67850a5603b2b
samoshsamosh <samoshkinamaria@gmail.com>
2016-10-17 22:36:31 +0300

    my first commit

---
+++ home.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$('.parent-menu').click(function(){
+		$('.type').slideToggle('slow');
+	});
+
+	// $('#parent-menu2').click(function(){
+	// 	$('.type2').slideToggle('slow', function(){
+
+	// 	});
+
+	// });
+ // 	$('#parent-menu3').click(function(){
+	// 	$('.type3').slideToggle('slow', function(){
+
+	// 	});
+	// });
+});
